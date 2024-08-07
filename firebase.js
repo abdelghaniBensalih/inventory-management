2ef2aa7852db628e9d3b84fb0eb1998f6d21e1b5
Abdelghani Bensalih <111251662+abdelghaniBensalih@users.noreply.github.com>
2024-08-07 10:51:51 +0000

    added UI for add, reduce and remove items

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore }  from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,12 +14,12 @@ const firebaseConfig = {
   storageBucket: "inventory-management-db48b.appspot.com",
   messagingSenderId: "387171182248",
   appId: "1:387171182248:web:4d6feeecd3f1792466c49d",
-  measurementId: "G-12XCMFXNLJ"
+  measurementId: "G-12XCMFXNLJ",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const firestore = getFirestor(app)
+const firestore = getFirestore(app);
 
-export { firestore }
+export { firestore };
